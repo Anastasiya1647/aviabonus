625edaa2116b45bf363bb33ed7aceb849eb90bb7
Anastasiia Levchenko <a.e.pyko@gmail.com>
2023-09-28 22:07:32 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 25654; // стоимость билета
+        int bonus = 20; // количество рублей для одной бонусной мили
+
+        System.out.println("За покупку билета начислено милей:" + (amount / bonus));
+            }
+}
